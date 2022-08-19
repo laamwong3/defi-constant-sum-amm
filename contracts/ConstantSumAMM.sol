57bdd88ac574f2e6899b6ed69175d3d3d9975082
Laam Wong <laamwong@Laams-iMac.local>
2022-08-19 20:02:23 +0800

    done ERC20

--- contracts/ConstantSumAMM.sol
+++ contracts/ConstantSumAMM.sol
@@ -1,2 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract ConstantSumAMM {
+    IERC20 public immutable tokenA;
+    IERC20 public immutable tokenB;
+
+    // keep track of token A and B
+    uint public reserveA;
+    uint public reserveB;
+
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+
+    constructor(address _tokenA, address _tokenB) {
+        tokenA = IERC20(_tokenA);
+        tokenB = IERC20(_tokenB);
+    }
+
+    function _mint(address _to, uint _amount) private {
+        balanceOf[_to] += _amount;
+        totalSupply += _amount;
+    }
+
+    function _burn(address _from, uint _amount) private {
+        balanceOf[_from] -= _amount;
+        totalSupply -= _amount;
+    }
+
+    function swap() external {}
+
+    function addLiquidity() external {}
+
+    function removeLiquidity() external {}
+}
