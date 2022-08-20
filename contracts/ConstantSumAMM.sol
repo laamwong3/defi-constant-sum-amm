f19eb6fac94b185f2e0148f99a3e66303cd7bf82
Laam Wong <laamwong3@gmail.com>
2022-08-20 08:26:56 +0800

    created contract

--- contracts/ConstantSumAMM.sol
+++ contracts/ConstantSumAMM.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+error ConstantSumAMM__InvalidToken();
+error ConstantSumAMM__SharesLessThanZero();
+
 contract ConstantSumAMM {
     IERC20 public immutable tokenA;
     IERC20 public immutable tokenB;
@@ -29,9 +32,94 @@ contract ConstantSumAMM {
         totalSupply -= _amount;
     }
 
-    function swap() external {}
+    /**
+     * @notice
+     */
+    function swap(address _tokenIn, uint _amountIn)
+        external
+        returns (uint amountOut)
+    {
+        // check if it's valid token
+        if (_tokenIn != address(tokenA) || _tokenIn != address(tokenB)) {
+            revert ConstantSumAMM__InvalidToken();
+        }
 
-    function addLiquidity() external {}
+        bool isTokenA = _tokenIn == address(tokenA);
+        (
+            IERC20 tokenIn,
+            IERC20 tokenOut,
+            uint reserveIn,
+            uint reserveOut
+        ) = isTokenA
+                ? (tokenA, tokenB, reserveA, reserveB)
+                : (tokenB, tokenA, reserveB, reserveA);
 
-    function removeLiquidity() external {}
+        // trasfer token to this contract
+        tokenIn.transferFrom(msg.sender, address(this), _amountIn);
+        uint amountIn = tokenIn.balanceOf(address(this)) - reserveIn;
+
+        // calculate the amount out with fees
+        // dx = dy
+        // 0.3% fee
+        amountOut = (amountIn * 997) / 1000;
+
+        // update reserveA or reserveB
+        (uint resA, uint resB) = isTokenA
+            ? (reserveIn + _amountIn, reserveOut - amountOut)
+            : (reserveOut - amountOut, reserveIn + _amountIn);
+        reserveA = resA;
+        reserveB = resB;
+
+        // transfer token out to user
+        tokenOut.transfer(msg.sender, amountOut);
+    }
+
+    /**
+     * shares = ((dA + dB) * T) / (A + B)
+     */
+    function addLiquidity(uint _amountA, uint _amountB)
+        external
+        returns (uint shares)
+    {
+        // make some transfer first
+        tokenA.transferFrom(msg.sender, address(this), _amountA);
+        tokenB.transferFrom(msg.sender, address(this), _amountB);
+
+        uint balanceA = tokenA.balanceOf(address(this));
+        uint balanceB = tokenB.balanceOf(address(this));
+
+        uint dA = balanceA - reserveA;
+        uint dB = balanceB - reserveB;
+
+        if (totalSupply == 0) {
+            shares = dA + dB;
+        } else {
+            shares = ((dA + dB) * totalSupply) / (reserveA + reserveB);
+        }
+
+        if (shares < 0) revert ConstantSumAMM__SharesLessThanZero();
+        _mint(msg.sender, shares);
+        reserveA = balanceA;
+        reserveB = balanceB;
+    }
+
+    /**
+     * dA = shares * A / T
+     * dB = shares * B / T
+     */
+    function removeLiquidity(uint _shares) external returns (uint dA, uint dB) {
+        dA = (_shares * reserveA) / totalSupply;
+        dB = (_shares * reserveB) / totalSupply;
+
+        _burn(msg.sender, _shares);
+        reserveA -= dA;
+        reserveB -= dB;
+
+        if (dA > 0) {
+            tokenA.transfer(msg.sender, dA);
+        }
+        if (dB > 0) {
+            tokenB.transfer(msg.sender, dB);
+        }
+    }
 }
