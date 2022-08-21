d2837c769ef8833186a74efe3cbf62f544ead262
Laam Wong <laamwong@Laams-iMac.local>
2022-08-21 19:12:02 +0800

    create to do

--- test/index.ts
+++ test/index.ts
@@ -1,2 +1,7 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+/**
+ * todos -> test the swap function
+ * todos -> test the addliquidity()
+ * todos -> test the removeLiquidity()
+ */
