6a88a3f73e9ad6a81ee6851b54e87d78b4d9ad2f
Laam Wong <laamwong3@gmail.com>
2022-08-22 13:53:17 +0800

    final test

--- test/index.ts
+++ test/index.ts
@@ -27,6 +27,8 @@ describe("ConstantSumAMM.sol", () => {
   let ConstantSumAMM: ConstantSumAMM__factory;
   let constantSumAMM: ConstantSumAMM;
   const mintAmount = 1000;
+  const tokenASupply = 1000;
+  const tokenBSupply = 1000;
 
   before(async () => {
     // deploy all contracts
@@ -62,8 +64,7 @@ describe("ConstantSumAMM.sol", () => {
 
   it("Should add liquidity", async () => {
     // user1 will provide liquidity to the pool
-    const tokenASupply = 1000;
-    const tokenBSupply = 1000;
+
     await constantSumAMM
       .connect(user1)
       .addLiquidity(tokenASupply, tokenBSupply);
@@ -78,5 +79,74 @@ describe("ConstantSumAMM.sol", () => {
     expect(await constantSumAMM.reserveB()).to.equal(tokenBSupply);
     expect(await constantSumAMM.tokenA()).to.equal(tokenA.address);
     expect(await constantSumAMM.tokenB()).to.equal(tokenB.address);
+  });
+
+  it("Should swap token from token A to token B", async () => {
+    //user2 will swap tokenA -> tokenB
+    const swapAmount = 1000;
+    const reserveABefore = await constantSumAMM.reserveA();
+    const reserveBBefore = await constantSumAMM.reserveB();
+    const user2BalanceBefore = await tokenB
+      .connect(user2)
+      .balanceOf(user2.address);
+    await constantSumAMM.connect(user2).swap(tokenA.address, swapAmount);
+
+    const reserveAAfter = await constantSumAMM.reserveA();
+    const reserveBAfter = await constantSumAMM.reserveB();
+    expect(reserveAAfter).equal(Number(reserveABefore) + swapAmount);
+    // 0.3% fees
+    expect(reserveBAfter).equal(
+      Number(reserveBBefore) - (Number(swapAmount) * 997) / 1000
+    );
+    // user2's token B should receive money included fees
+    const user2BalanceAfter = await tokenB
+      .connect(user2)
+      .balanceOf(user2.address);
+    const user2ReceiveAmount = (swapAmount * 997) / 1000;
+    expect(user2BalanceAfter).equal(
+      user2ReceiveAmount + Number(user2BalanceBefore)
+    );
+  });
+  it("Should swap token from token B to token A", async () => {
+    //user2 will swap tokenB -> tokenA
+    const swapAmount = 1000;
+    const reserveABefore = await constantSumAMM.reserveA();
+    const reserveBBefore = await constantSumAMM.reserveB();
+    // console.log(reserveABefore, reserveBBefore);
+    const user2BalanceBefore = await tokenA
+      .connect(user2)
+      .balanceOf(user2.address);
+    await constantSumAMM.connect(user2).swap(tokenB.address, swapAmount);
+
+    const reserveAAfter = await constantSumAMM.reserveA();
+    const reserveBAfter = await constantSumAMM.reserveB();
+    // console.log(reserveAAfter, reserveBAfter);
+    expect(reserveBAfter).equal(Number(reserveBBefore) + swapAmount);
+    // 0.3 % fees;
+    expect(reserveAAfter).equal(
+      Number(reserveABefore) - (Number(swapAmount) * 997) / 1000
+    );
+    // user2's token A should receive money included fees
+    const user2BalanceAfter = await tokenA
+      .connect(user2)
+      .balanceOf(user2.address);
+    const user2ReceiveAmount = (swapAmount * 997) / 1000;
+    expect(user2BalanceAfter).equal(
+      user2ReceiveAmount + Number(user2BalanceBefore)
+    );
+  });
+  it("Should remove liquidity", async () => {
+    // only user1 add liquidity, so user1 will have 100% shares
+    // assume platform has no charge
+    const removeLiquidityShares = tokenASupply + tokenBSupply;
+    const reserveABefore = await constantSumAMM.reserveA();
+    const reserveBBefore = await constantSumAMM.reserveB();
+    await constantSumAMM.connect(user1).removeLiquidity(removeLiquidityShares);
+
+    expect(await constantSumAMM.reserveA()).equal(0);
+    expect(await constantSumAMM.reserveB()).equal(0);
+
+    expect(await tokenA.balanceOf(user1.address)).equal(reserveABefore);
+    expect(await tokenB.balanceOf(user1.address)).equal(reserveBBefore);
   });
 });
